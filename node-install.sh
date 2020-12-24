35995092a39e22e8d198924321fbd14438e7e392
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2020-12-24 15:33:19 +0530

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -11,6 +11,38 @@ sudo apt install -y ./casper-client_0.3.1-3423_amd64.deb
 sleep 3
 casper-client keygen /etc/casper/validator_keys/
 sleep 3
+sudo apt install jq
+sleep 3
+sudo apt-get update && sudo apt-get install make
+sleep 3
+curl https://sh.rustup.rs -sSf | sh
+sleep 3
+sudo apt install build-essential
+sleep 3
+curl -JLO https://raw.githubusercontent.com/matsuro-hadouken/casper-tools/master/balance_check.sh
+sleep 3
+curl -JLO https://raw.githubusercontent.com/matsuro-hadouken/casper-tools/master/bond.sh
+sleep 3
+chmod 755 balance_check.sh
+sleep 3
+chmod 755 bond.sh
+sleep 3
+./balance_check.sh $(cat /etc/casper/validator_keys/public_key_hex)
+sleep 3
+source $HOME/.cargo/env
+sleep 3
+git clone https://github.com/CasperLabs/casper-node
+sleep 3
+cd casper-node
+sleep 3
+git checkout v0.2.1
+sleep 3
+make setup-rs
+sleep 3
+make build-client-contracts -j
+sleep 3
+make build-system-contracts -j
+sleep 3
 curl -s http://54.67.67.33:8888/status | jq -r .last_added_block_info.hash
 sleep 3
 sudo logrotate -f /etc/logrotate.d/casper-node
