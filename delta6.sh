b9d6704c65efb6ba4ed6574d29e56edff6179120
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2020-12-30 21:08:23 +0530

    Update delta6.sh

--- delta6.sh
+++ delta6.sh
@@ -13,3 +13,13 @@ sudo apt install -y ./casper-node_0.4.0-3480_amd64.deb ./casper-client_0.4.0-348
 
 # Get trusted hash for config.toml
 curl -s 18.144.176.168:8888/status | jq -r .last_added_block_info.hash
+a1="$(curl -s 18.144.176.168:8888/status | jq -r .last_added_block_info.hash)"
+sed -i 's/# trusted_hash =/trusted_hash = '"'$a1'"'/' /etc/casper/config.toml
+sudo logrotate -f /etc/logrotate.d/casper-node
+sudo /etc/casper/delete_local_db.sh
+sudo systemctl stop casper-node
+sudo systemctl start casper-node
+systemctl status casper-node
+ctrl+c
+sed -i 's/add_bid/do_bid/g' bond.sh
+./bond.sh
