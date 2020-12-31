a0d143b2d1d971fc1eed84d4326d171e5090b10a
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2020-12-31 17:34:47 +0530

    Update delta7.sh

--- delta7.sh
+++ delta7.sh
@@ -10,8 +10,7 @@ sudo apt remove -y casper-client casper-node
 curl -JLO https://bintray.com/casperlabs/debian/download_file?file_path=casper-client_0.4.0-3483_amd64.deb
 curl -JLO https://bintray.com/casperlabs/debian/download_file?file_path=casper-node_0.4.0-3483_amd64.deb
 
-sudo apt install -y ./casper-node_0.4.0-3483_amd64.deb 
-sudp apt install -y ./casper-client_0.4.0-3483_amd64.deb 
+sudo apt install -y ./casper-node_0.4.0-3483_amd64.deb ./casper-client_0.4.0-3483_amd64.deb 
 
 # Get trusted hash for config.toml
 curl -s 18.144.176.168:8888/status | jq -r .last_added_block_info.hash
