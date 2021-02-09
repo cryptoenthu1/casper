28a0549ffc79b56c53010ae86457c58e4446b849
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2021-02-09 17:26:06 +0530

    Update delta10.sh

--- delta10.sh
+++ delta10.sh
@@ -23,8 +23,10 @@ sudo -u casper ./pull_casper_node_version.sh 1_0_0
 
 # Get trusted_hash into config.toml
 curl -s 18.144.176.168:8888/status | jq -r .last_added_block_info.hash
-a1="$(curl -s 18.144.176.168:8888/status | jq -r .last_added_block_info.hash)"
-sed -i 's/# trusted_hash =/trusted_hash = '"'$a1'"'/' /etc/casper/config.toml
+
+# Or do this automatically
+sudo sed -i "/trusted_hash =/c\trusted_hash = '$(curl -s 18.144.176.168:8888/status | jq -r .last_added_block_info.hash | tr -d '\n')'" /etc/casper/1_0_0/config.toml
+
 sudo logrotate -f /etc/logrotate.d/casper-node
 sudo /etc/casper/delete_local_db.sh; sleep 1
 sudo systemctl stop casper-node-launcher
