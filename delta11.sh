95a5795d3c08848b4a6e9e01dc42a482c71b0ccf
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2021-03-15 11:41:23 +0530

    Update delta11.sh

--- delta11.sh
+++ delta11.sh
@@ -1,13 +1,22 @@
-sudo systemctl stop casper-node-launcher
+# clean up previous version nodes
+sudo systemctl stop casper-node-launcher.service
+
 sudo rm -rf /etc/casper/1_0_0
 sudo rm -rf /var/lib/casper/bin/1_0_0
+sudo rm -rf /var/lib/casper/casper-node
+sudo rm /etc/casper/casper-node-launcher-state.toml
 
-cd /etc/casper
-sudo -u casper ./pull_casper_node_version.sh 1_0_0
+sudo apt remove -y casper-client casper-node-launcher
 
-sudo sed -i "/trusted_hash =/c\trusted_hash = '$(curl -s 13.57.181.239:8888/status | jq -r .last_added_block_info.hash | tr -d '\n')'" /etc/casper/1_0_0/config.toml
+cd ~
+curl -JLO https://bintray.com/casperlabs/debian/download_file?file_path=casper-client_0.9.3-0_amd64.deb
+curl -JLO https://bintray.com/casperlabs/debian/download_file?file_path=casper-node-launcher_0.3.1-0_amd64.deb
+
+sudo apt install -y ./casper-client_0.9.3-0_amd64.deb ./casper-node-launcher_0.3.1-0_amd64.deb
+
+sudo -u casper /etc/casper/pull_casper_node_version.sh 1_0_0 delta-11
+sudo -u casper /etc/casper/config_from_example.sh 1_0_0
 
 sudo logrotate -f /etc/logrotate.d/casper-node
-sudo /etc/casper/delete_local_db.sh; sleep 1
-sudo systemctl start casper-node-launcher
+sudo systemctl start casper-node-launcher; sleep 2
 systemctl status casper-node-launcher
